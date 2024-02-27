f823d77c47078c03d2c858514b312049eebb4637
Julio C. López <55455995+JulioLopez27@users.noreply.github.com>
2024-02-27 01:14:16 -0300

    Delete src directory

--- src/server.js
+++
@@ -1,5 +0,0 @@
-//importa toda la aplicacion(config) del servicio a ser ejecutado
-import { app } from './setup.js'
-//crea e instacia el servicio que queda ejecutandose todo el tiempo
-//para ejecutar el servidor en el puerto 3000
-app.listen(3000)
